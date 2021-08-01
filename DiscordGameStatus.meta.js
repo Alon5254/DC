2e6e6470c80eece8f698f6b6df5157ba7e8aeb1e
Alon5254 <88267240+Alon5254@users.noreply.github.com>
2021-08-01 09:54:32 +0200

    Add files via upload

---
+++ DiscordGameStatus.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @author      PotcFdk
+// @namespace   https://github.com/PotcFdk
+// @name        DiscordGameStatus
+// @description A userscript for setting the currently playing game in the Discord web client
+// @include     https://discord.com/*
+// @version     1.7.1
+// @grant       GM_unsafeWindow
+// @run-at      document-end
+// @downloadURL https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.user.js
+// @updateURL   https://raw.githubusercontent.com/PotcFdk/DiscordGameStatus/master/DiscordGameStatus.meta.js
+// ==/UserScript==
